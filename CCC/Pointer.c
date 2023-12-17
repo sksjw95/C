30c6ac1dddb9acba574855f04a58aac0a08784ac
sksjw95 <tjwldnd95@hanmail.net>
2023-12-17 15:21:47 +0900

    Pointer

---
+++ CCC/Pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main() {
+	char data[5];
+	data[1] = 5;
+	*(data + 1) = 5;
+	printf("%d\n", data[1]);
+	printf("%d", *(data + 1));
+
+	char data;
+	char* p = &data;
+	*p = 3;
+	printf("%d\n", *p);
+	p[0] = 3;
+	printf("%d", p[0]);
+
+}
