77b8fb56ae857f291e98b8fa6755f8b7af43095c
sksjw95 <tjwldnd95@hanmail.net>
2023-12-18 15:10:29 +0900

    Pointer2

---
+++ CCC/Pointer2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main() {
+	char data[5] = { 1,2,3,4,5 };
+	int i, sum = 0, select = 2;
+	/*for (i = 0; i < 10; i++) {
+		sum = sum + data[select];
+	}
+	printf("%d\n", sum);*/
+	for (i = 0; i < 10; i++) {
+		sum = sum + *(data + 2);
+	}
+	printf("%d\n", sum);
+
+	char* p = data + select;
+	for (i = 0; i < 10; i++) {
+		sum = sum + *p;
+	}
+	printf("%d\n", sum);
+}
