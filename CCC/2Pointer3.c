442e99e90204b323e8089beaa98334116224b5f1
sksjw95 <tjwldnd95@hanmail.net>
2024-01-01 23:58:18 +0900

    2Pointer3

---
+++ CCC/2Pointer3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <malloc.h>
+void main() {
+	short** pp;
+	pp = (short**)malloc(sizeof(short*));
+	*pp = (short*)malloc(sizeof(short));
+
+	**pp = 10;
+	printf("**pp : %d\n", **pp);
+	free(*pp);
+	free(pp);
+}
