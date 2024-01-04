4db39ba693dd1258680ae4ace12c524410f3fce7
sksjw95 <tjwldnd95@hanmail.net>
2024-01-04 09:50:27 +0900

    2Pointer6

---
+++ CCC/2Pointer6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main() {
+	unsigned char limit_table[3] = { 4,2,3 };
+	unsigned char count[3][4];
+	int age, member, temp, sum;
+	for (age = 0; age < 3; age++) {
+		printf("\n%d´ëÀÇ ¿¬·ÉÀÇ À­¸ö ÀÏÀ¸Å°±â È½¼ö\n", age + 2);
+		for (member = 0; member < limit_table[age]; member++) {
+			printf("%dth : ", member + 1);
+			scanf_s("%d", &temp, sizeof(temp));
+			count[age][member] = (unsigned char)temp;
+		}
+	}
+	printf("\n\n¿¬·Éº° Æò±Õ À­¸ö ÀÏÀ¸Å°±â È½¼ö\n");
+	for (age = 0; age < 3; age++) {
+		sum = 0;
+		printf("%d0´ë : ", age + 2);
+		for (member = 0; member < limit_table[age]; member++) {
+			sum = sum + count[age][member];
+		}
+		printf("%5.2f\n", (double)sum / limit_table[age]);
+	}
+}
