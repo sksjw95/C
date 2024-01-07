28ea0c20a03784284895dce08bde0d245d88457a
sksjw95 <tjwldnd95@hanmail.net>
2024-01-07 13:27:02 +0900

    Fgets

---
+++ CCC/Fgets.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main() {
+	char temp[64];
+	FILE* p_file = fopen("C:\\SJW\\tips.txt", "rt");
+	if (NULL != p_file) {
+		while (NULL != fgets(temp, sizeof(temp), p_file)) {
+			printf("%s", temp);
+		}
+		fclose(p_file);
+	}
+}
