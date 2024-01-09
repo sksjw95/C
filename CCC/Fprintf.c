6be67b6532be7b3e105065f2722b829c81ccf937
sksjw95 <tjwldnd95@hanmail.net>
2024-01-10 00:13:57 +0900

    Fprintf

---
+++ CCC/Fprintf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main() {
+	FILE* p_file = fopen("C:\\SJW\\tipssoft.txt", "wt");
+	if (NULL != p_file) {
+		short int data = 412;
+		fprintf(p_file, "Hello\n");
+		fprintf(p_file, "%d\n", data);
+		fclose(p_file);
+	}
+}
