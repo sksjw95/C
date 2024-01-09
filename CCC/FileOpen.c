12bee3b4a54d614b3ac6a170d4282bca9c69263c
sksjw95 <tjwldnd95@hanmail.net>
2024-01-09 23:49:23 +0900

    FileOpen

---
+++ CCC/FileOpen.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main() {
+	FILE* p_file = fopen("C:\\SJW\\c.txt", "rt");
+	if (p_file != NULL) {
+		printf("1");
+	}
+	else {
+		printf("파일이 존재하지 않거나 열 수 없습니다.\n");
+	
+	}
+};
