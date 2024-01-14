24a3666766965aedd57cdfb34a865db189fe3f27
sksjw95 <tjwldnd95@hanmail.net>
2024-01-14 13:43:23 +0900

    Fread2

---
+++ CCC/Fread2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main() {
+	int file_size = 0;
+	FILE* p_file = fopen("C:\\SJW\\tipssoft", "rb");
+	if (NULL != p_file) {
+		fseek(p_file, 0, SEEK_END);
+		file_size = ftell(p_file);
+		printf("파일 크기 : %d\n", file_size);
+		fclose(p_file);
+	}
+}
