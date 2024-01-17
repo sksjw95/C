973fbf3be98aec52b854d5294247e118d8a28497
sksjw95 <tjwldnd95@hanmail.net>
2024-01-17 22:00:38 +0900

    Fwrite

---
+++ CCC/Fwrite.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main() {
+	int data = 0x00000412;
+	FILE* p_file = fopen("C:\\SJW\\jin.dat", "wb");
+	if (NULL != p_file) {
+		fwrite(&data, sizeof(int), 1, p_file);
+		fclose(p_file);
+	}
+}
