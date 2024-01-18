a21580fe33dd82862d6202cb2275ede930136191
sksjw95 <tjwldnd95@hanmail.net>
2024-01-18 09:13:45 +0900

    Fwrite2

---
+++ CCC/Fwrite2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main() {
+	int data = 100, data_list[5] = { 0,1,2,3,4 };
+	FILE* p_file = fopen("C:\\SJW\\tipssoft", "wb");
+	if (NULL != p_file) {
+		fwrite(&data, sizeof(int), 1, p_file);
+		fwrite(data_list, sizeof(int), 5, p_file);
+		fclose(p_file);
+	}
+}
