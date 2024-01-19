ccae690701df2a20417971d32d997150b3e5b35a
sksjw95 <tjwldnd95@hanmail.net>
2024-01-19 11:31:01 +0900

    Struct

---
+++ CCC/Struct.c
@@ -0,0 +1,26 @@
+struct People
+{
+	char name[12];
+	unsigned short int age;
+	float height;
+	float weight;
+};
+typedef struct People Person;
+
+
+struct People
+{
+	char name[12];
+	unsigned short int age;
+	float height;
+	float weight;
+}Person;
+
+
+typedef struct
+{
+	char name[12];
+	unsigned short int age;
+	float height;
+	float weight;
+}Person;
