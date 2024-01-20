95a2a52801f28f951e3df302641ec1c3156e71eb
sksjw95 <tjwldnd95@hanmail.net>
2024-01-20 12:37:16 +0900

    Typedef

---
+++ CCC/Typedef.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef unsigned short int US;
+
+void main() {
+	unsigned short int data = 5;
+	US temp;
+
+	temp = data;
+	char a = 'c';
+	printf("temp = %d\n", temp);
+	printf("%d", sizeof(a));
+}
