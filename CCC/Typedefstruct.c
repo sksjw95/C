e0225966999f93d7b5ee2d5a13876aa5a311bff1
sksjw95 <tjwldnd95@hanmail.net>
2024-01-21 00:07:33 +0900

    Typedefstruct

---
+++ CCC/Typedefstruct.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+typedef struct People {
+	char name[12];
+	unsigned short int age;
+	float height;
+	float weight;
+}Person;
+
+void main() {
+	Person data;
+
+	data.age = 21;
+	data.height = 178.3;
+}
